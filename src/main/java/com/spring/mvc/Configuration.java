1842c69d81f2d266f177ec918fc292ec71b89dd5
Dmytro Ileiko <deamon999@gmail.com>
2022-12-07 09:40:38 -0600

    fixed thymeleaf configuration

--- src/main/java/com/spring/mvc/Configuration.java
+++ src/main/java/com/spring/mvc/Configuration.java
@@ -9,11 +9,12 @@ import com.spring.mvc.service.TicketService;
 import com.spring.mvc.service.UserService;
 import com.spring.mvc.storage.Storage;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Description;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -22,19 +23,13 @@ import org.thymeleaf.templatemode.TemplateMode;
 @org.springframework.context.annotation.Configuration
 @EnableWebMvc
 @ComponentScan("com.spring.mvc")
-public class Configuration {
+public class Configuration implements WebMvcConfigurer, ApplicationContextAware {
 
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+    private ApplicationContext applicationContext;
 
-        // set location of views.
-        vr.setPrefix("/WEB-INF/view/");
-
-        // set the extension of views.
-        vr.setSuffix(".jsp");
-
-        return vr;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
     }
 
     @Bean
@@ -77,47 +72,58 @@ public class Configuration {
         return new BookingFacade(eventService(), userService(), ticketService());
     }
 
-//    @Bean
-//    @Description("Thymeleaf Template Resolver")
-//    public SpringResourceTemplateResolver templateResolver(ApplicationContext applicationContext) {
-//        // SpringResourceTemplateResolver automatically integrates with Spring's own
-//        // resource resolution infrastructure, which is highly recommended.
-//        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-//        templateResolver.setApplicationContext(applicationContext);
-//        templateResolver.setPrefix("/WEB-INF/view/");
+    //    @Bean
+//    public InternalResourceViewResolver viewResolver() {
+//        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+//
+//        // set location of views.
+//        vr.setPrefix("/WEB-INF/view/");
+//
+//        // set the extension of views.
+//        vr.setSuffix(".jsp");
+//
+//        return vr;
+//    }
+
+    @Bean
+    @Description("Thymeleaf Template Resolver")
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/view/");
 //        templateResolver.setSuffix(".html");
-//        // HTML is the default value, added here for the sake of clarity.
-//        templateResolver.setTemplateMode(TemplateMode.HTML);
-//        // Template cache is true by default. Set to false if you want
-//        // templates to be automatically updated when modified.
-//        templateResolver.setCacheable(true);
-//        return templateResolver;
-//    }
-//
-//    @Bean
-//    @Description("Thymeleaf Template Engine")
-//    public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
-//        // SpringTemplateEngine automatically applies SpringStandardDialect and
-//        // enables Spring's own MessageSource message resolution mechanisms.
-//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-//        templateEngine.setTemplateResolver(templateResolver);
-//        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
-//        // speed up execution in most scenarios, but might be incompatible
-//        // with specific cases when expressions in one template are reused
-//        // across different data types, so this flag is "false" by default
-//        // for safer backwards compatibility.
-//        templateEngine.setEnableSpringELCompiler(true);
-//        return templateEngine;
-//    }
-//
-//    @Bean
-//    @Description("Thymeleaf View Resolver")
-//    public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
-//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-//        viewResolver.setTemplateEngine(templateEngine);
-//        // NOTE 'order' and 'viewNames' are optional
-//        viewResolver.setOrder(1);
-//        viewResolver.setViewNames(new String[]{".html", ".xhtml"});
-//        return viewResolver;
-//    }
+        // HTML is the default value, added here for the sake of clarity.
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        // Template cache is true by default. Set to false if you want
+        // templates to be automatically updated when modified.
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    @Bean
+    @Description("Thymeleaf Template Engine")
+    public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+        // SpringTemplateEngine automatically applies SpringStandardDialect and
+        // enables Spring's own MessageSource message resolution mechanisms.
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+        // speed up execution in most scenarios, but might be incompatible
+        // with specific cases when expressions in one template are reused
+        // across different data types, so this flag is "false" by default
+        // for safer backwards compatibility.
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Bean
+    @Description("Thymeleaf View Resolver")
+    public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        // NOTE 'order' and 'viewNames' are optional
+        viewResolver.setOrder(1);
+        viewResolver.setViewNames(new String[]{"*.html", "*.xhtml"});
+        return viewResolver;
+    }
 }
--- src/main/java/com/spring/mvc/controller/UserController.java
+++ src/main/java/com/spring/mvc/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.spring.mvc.controller;
 
+import com.spring.mvc.dao.UserImpl;
+import com.spring.mvc.facade.BookingFacade;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final BookingFacade bookingFacade;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String createUser() {
-        return "first-view";
+        bookingFacade.createUser(new UserImpl("test", 1L, "test@test.com"));
+        return "first-view.html";
     }
 }
